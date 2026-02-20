92db7e8999a25acf04059b382a887538043440ff
ClementJeanjean <clemjj01@gmail.com>
2026-02-20 12:03:51 +0100

    Update load_geojson.R

--- src/load_geojson.R
+++ src/load_geojson.R
@@ -1,7 +1,7 @@
 
-# =========================== #
-# LECTURE GEOJSON
-# =========================== #
+# ===========================
+# READ GEOJSON
+# ===========================
 
 root_dir <- getwd()
 data_dir <- file.path(root_dir, "data")
@@ -10,4 +10,5 @@ df_geojson <- read_sf(file.path(data_dir, filename))
 df_geojson %>% rename(pays_en = name)
 
 df_all <- merge(x=df_geojson, y=df_data, by.x="name", by.y="pays_en")
+
 
